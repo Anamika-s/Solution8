3505c85418d0bdf5e7ec82eaca7dea9e59796ce1
Anamika Sawhney <anamika.sawhney22@gmail.com>
2025-08-05 12:17:14 +0530

    Add project files.

---
+++ ADONetDemo/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Data.SqlClient;
+namespace ADONetDemo
+{
+    internal class Program
+    {
+        static int Menu()
+        {
+            Console.WriteLine("1. Read Record");
+            Console.WriteLine("2. Add Record");
+            Console.WriteLine("3. Delete Record");
+            Console.WriteLine("4. Edit Record");
+            Console.WriteLine("enter choice");
+            int ch = byte.Parse(Console.ReadLine());
+            return ch;
+        }
+        static void Main(string[] args)
+        {
+            string choice = "y";
+            while (choice == "y")
+            {
+                int c = Menu();
+                switch (c)
+                {
+                    case 1: GetRecords(); break;
+                    case 2: AddRecord(); break;
+                    case 3: DeleteRecord(); break;
+                    case 4: EditRecord(); break;    
+                }
+                Console.WriteLine("Repeat"); choice = Console.ReadLine();
+            }
+
+        }
+        static void GetRecords()
+        {
+
+
+            // connectionString contains details abt the database with you want to connect
+            // it takes some para
+            string connectioString = @"server=ANAMIKA\SQLSERVER;database=demo;integrated security=true";
+            SqlConnection connection = new SqlConnection(connectioString);
+
+            SqlCommand command = new SqlCommand("select * from student", connection);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0] + " " + reader[1] + " " + reader[2]);
+                }
+                reader.Close();
+                connection.Close();
+            }
+            else
+            {
+                Console.WriteLine("There are no records");
+
+            }
+        }
+         
+     static void AddRecord()
+    {
+            string connectioString = @"server=ANAMIKA\SQLSERVER;database=demo;integrated security=true";
+            SqlConnection connection = new SqlConnection(connectioString);
+            SqlCommand command = new SqlCommand("insert into student(name , address, marks) values ('deepak', 'delhi', 90)", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        static void DeleteRecord()
+        {
+            string connectioString = @"server=ANAMIKA\SQLSERVER;database=demo;integrated security=true";
+            SqlConnection connection = new SqlConnection(connectioString);
+            SqlCommand command = new SqlCommand("delete  student where rn =1", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        static void EditRecord()
+        {
+            string connectioString = @"server=ANAMIKA\SQLSERVER;database=demo;integrated security=true";
+            SqlConnection connection = new SqlConnection(connectioString);
+            SqlCommand command = new SqlCommand("update  student set name ='newname' where rn =1", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+                }
+    }
+}
+
---
+++ LINQDemos/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LINQDemos
+{
+    class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+    }
+    class Batch
+    {
+        public int BatchId { get; set; }
+        public string BatchName { get; set; }
+        public DateTime StartDate { get; set; }
+        public int CourseId { get; set; }
+    }
+
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Course> courses = new List<Course>()
+           {
+             new Course { Id = 1,Name="DotNet", Details="Freshers Course" },
+             new Course { Id = 2,Name="Adv DotNet", Details="Inter Course" },
+             new Course { Id = 3,Name="Java", Details="Freshers Course" },
+             new Course { Id = 4,Name="Adv Java", Details="Freshers Course" }
+
+           };
+            List<Batch> batches = new List<Batch>() { 
+                new Batch() {BatchId = 1, BatchName ="B001", CourseId=1 , StartDate = DateTime.Parse("12/12/2025")},
+                new Batch() {BatchId = 2, BatchName ="B002", CourseId=2 , StartDate = DateTime.Parse("12/12/2025")},
+                new Batch() {BatchId = 3, BatchName ="B003", CourseId=4 , StartDate = DateTime.Now},
+
+            };
+
+            // display batch details along with course details
+            var batchDetails = (from batch in batches
+                                join course in courses
+                                 on batch.CourseId equals course.Id
+                                select new { batch.BatchId, batch.BatchName, batch.StartDate, course.Id, course.Name, course.Details }).ToList();
+            Console.WriteLine("Details are ");
+            Console.WriteLine($"Batch ID\tBAtch Name\t STart DAte\tCourse ID\t Course NAme ");
+            foreach (var batch in batchDetails)
+            {
+                Console.WriteLine($"{batch.BatchId}\t{batch.BatchName}\t{batch.StartDate}\t{batch.Id}\t{batch.Name}\t{batch.Details}");
+
+            }
+        }
+    }
+}
